a8e72d1e27b6e7cc3f8c4c54af55c8279583c725
[Ruliaga] <ruliaga@mail.ru>
2022-06-24 19:04:38 +0500

    Добавлен слушатель на кнопку

--- app/src/main/java/ru/evgenysidorov/androidkotlin_lesson1/MainActivity.kt
+++ app/src/main/java/ru/evgenysidorov/androidkotlin_lesson1/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.evgenysidorov.androidkotlin_lesson1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.button)
+        btn.setOnClickListener{
+            val toast=Toast.makeText(this,"Нажми ещё",Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
